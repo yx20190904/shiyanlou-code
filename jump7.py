4782fa3f228a61aad235d0ad92f5bdb37decff1d
yx20190904 <464651164@qq.com>
2019-09-12 14:20:09 +0800

    frist commit

---
+++ jump7.py
@@ -0,0 +1,18 @@
+
+a=0
+while a<100:
+    a=a+1
+    if a%7==0:
+        continue
+    if a%10==7:
+        continue
+    if a//10==7:
+        continue
+
+  ###  else a//10==7:
+  ###      continue
+    print(a)
+    ###a=a+1
+
+ ### a%7==0 or a%10==7 or a//10==7
+
